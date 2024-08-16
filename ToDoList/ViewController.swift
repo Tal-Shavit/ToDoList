6fd71b7f5e3f52f966fd30334d991dc9a6ab165f
Tal-Shavit <talshavit84@gmail.com>
2024-08-16 11:31:40 +0300

    view

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statesCardView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        statesCardView.layer.cornerRadius = 10
+            statesCardView.layer.shadowColor = UIColor.black.cgColor
+            statesCardView.layer.shadowOpacity = 0.2
+            statesCardView.layer.shadowOffset = CGSize(width: 0, height: 2)
+            statesCardView.layer.shadowRadius = 4
         //lllllllll
         // Do any additional setup after loading the view.
     }
