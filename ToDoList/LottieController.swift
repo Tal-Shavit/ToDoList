72e485079af3fc909b9b30388d03dc99afbd0bfd
Tal-Shavit <talshavir84@gmail.com>
2024-08-20 21:18:11 +0300

    lottie

---
+++ ToDoList/LottieController.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+import UIKit
+import Lottie
+
+class LottieController: UIViewController  {
+    
+    var animationView: AnimationView = .init()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //animationView.animation = Animation.named()
+        
+    }
+}
